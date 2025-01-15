4e8ab06c1a1b3c670cce263143686a8d1314aad4
Isadru Santos <kofianaan0@gmail.com>
2025-01-15 13:27:14 +0300

    Update AIChatBox.tsx

--- src/components/AIChatBox.tsx
+++ src/components/AIChatBox.tsx
@@ -138,6 +138,7 @@ export function ChatMessage({ message: { role, content } }: ChatMessageProps) {
             p: ({ node, ...props }) => (
               <p {...props} className="mt-3 first:mt-0" />
             ),
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             ul: ({ node, ...props }) => (
               <ul
                 {...props}
